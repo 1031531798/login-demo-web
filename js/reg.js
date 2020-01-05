f7c866716dcbe2b3ac27d19fa0f7792cbd9eff04
AhogeK <ahogek@gmail.com>
2020-01-05 06:49:37 -0800

    删除多余注释

--- js/reg.js
+++ js/reg.js
@@ -13,29 +13,10 @@ window.onload = function () {
             document.getElementById("email-box").innerHTML = "邮箱地址格式有误";
             return false;
         } else {
-            document.getElementById("email-box").innerHTML = ""
+            document.getElementById("email-box").innerHTML = "";
             return true;
         }
     }
-    //密码验证
-    // var password = document.getElementById('password');
-    // password.onblur = function() {
-    //     //创建正则表达式
-    //     var reg = /^(?![0-9]+$)(?![a-z]+$)(?![A-Z]+$)(?!([^(0-9a-zA-Z)])+$).{0,64}$/;
-    //     if (password.value === "") { //验证密码是否为空
-    //         document.getElementById("password-box").innerHTML = "密码不能为空";
-    //         return false;
-    //     } else if (!reg.test(password.value)) { //正则不通过，格式不对
-    //         document.getElementById("password-box").innerHTML = "密码必须包含 数字,英文,字符中的两种以上";
-    //         return false;
-    //     } else if (password.value.length < 8) { //密码不能小于8个字符
-    //         document.getElementById("password-box").innerHTML = "密码不能小于8位字符";
-    //         return false;
-    //     } else {
-    //         document.getElementById("password-box").innerHTML = "";
-    //         return true;
-    //     }
-    // }
     document.getElementById("login").onclick = function check() {
         //创建判断
         if (document.getElementById("email-box").innerHTML == "" && document.getElementById("password-box").innerHTML == "") {
@@ -55,9 +36,8 @@ window.onload = function () {
         } else {
             return false;
         }
-        //入场动画效果
-        document.getElementsByClassName('sign-up-form')[0].style.opacity = 1;
-        document.getElementsByClassName('sign-up-form')[0].style.top = 0 + 'px';
     }
-
+    //入场动画效果
+    document.getElementsByClassName('sign-up-form')[0].style.opacity = 1;
+    document.getElementsByClassName('sign-up-form')[0].style.top = 0 + 'px';
 }
