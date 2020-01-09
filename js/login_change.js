5db30dbfbb1d6db7f151fd3a6676c11c50c7ba35
AhogeK <ahogek@gmail.com>
2020-01-09 14:47:53 +0800

    完善登录请求

--- js/login_change.js
+++ js/login_change.js
@@ -1,43 +1,22 @@
 window.onload = function () {
-    function getUser() {
-        // console.log($('#login_form').serializeJSON());
-        // console.log(JSON.stringify($('#login_form').serializeJSON()));
-        return JSON.stringify($('#login_form').serializeJSON());
-    }
+    $("#login").click(function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'api/login/userLogin', true);
+        xhr.send(new FormData($("#login_form")[0]));
+        // Track the state changes of the request.
+        xhr.onreadystatechange = function () {
+            var DONE = 4; // readyState 4 means the request is done.
+            var OK = 200; // status 200 is a successful return.
+            if (xhr.readyState === DONE) {
+                if (xhr.status === OK) {
+                    var result = JSON.parse(xhr.responseText)
+                    alert(result.msg); // 'This is the output.'
+                } else {
+                    alert('Error: ' + xhr.status); // An error occurred during the request.
+                }
+            }
 
-    document.getElementById("login").onclick = function login_check() {
-            //AJAX部分
-            var data = getUser();
-            console.log(data)
-            // $.ajax({
-            //     type: "POST",
-            //     url: "api/login/userLogin",
-            //     data: data,
-            //     dataType: "json",
-            //     contentType: "application/json",
-            //     success: function (data) {
-            //         alert(data.message);
-            //     },
-            //     statusCode: {
-            //         403: function () {
-            //             document.getElementById("password-box").innerHTML = "<img src='img/error.png'>  邮箱或密码错误";
-            //             document.getElementById("password-box").style.color = "red";
-            //         }
-            //     }
-            // });//AJAX部分
-                var request = new XMLHttpRequest();
-                var form = document.getElementById('login_form');
-                var fd = new FormData(form);
-                request.addEventListener('load', function() {
-                    console.log('成功')
-                })
-                request.addEventListener('error', function() {
-                    console.log('失败')
-                })
-                request.open('POST', 'api/login/userLogin', true);
-                request.send(fd);
-                
-        } 
+        }
         entrance();
-    }
-   
+    })
+}
