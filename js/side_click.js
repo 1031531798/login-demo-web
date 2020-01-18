0ebadf9193e9c9c7fbb184d8e8effcf9d1a49e43
violet <1031531798@qq.com>
2020-01-18 23:23:41 +0800

    添加侧栏回到顶部  header动画

--- js/side_click.js
+++ js/side_click.js
@@ -9,13 +9,28 @@ function side_click() {
 		$('#' + this.id).css("background", "rgb(255,127,80)")
 		$('#' + this.id).css("fontWeight", "bolder")
 	}
+	var top = function (){};
 	//获取滚动条位置为以后侧栏定位做准备
 	$(window).scroll(function () {
-		var top = $(window).scrollTop();
-		if(top > 100){
-			$('#move_top').css('display',"block")
+		var dt = top.prototype.y;
+		console.log(dt)
+		top.prototype.y = $(window).scrollTop();
+		console.log(top.prototype.y)
+		if(dt > top.prototype.y){
+			$('#header').slideDown();
 		}else{
-			$('#move_top').css('display',"none")
+			$('#header').slideUp();
+		}
+		if(top.prototype.y > 100){
+			//取消动画滞后
+			$('#move_top').stop();
+			//下滑显示
+			$('#move_top').slideDown();
+		}else{
+			//取消动画滞后
+			$('#move_top').stop();
+			//上滑隐藏
+			$('#move_top').slideUp();
 		}
 	})
 	$('.side').click(style_change)
